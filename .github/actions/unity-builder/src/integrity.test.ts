17720f18e2cd4d403c8c04fdbc2afd21c80d6252
Mattia Belletti <mattia.belletti@gmail.com>
2022-08-28 15:15:45 +0200

    chore: removing local copy of unity builder.

--- .github/actions/unity-builder/src/integrity.test.ts
+++
@@ -1,9 +0,0 @@
-import { stat } from 'fs/promises';
-
-describe('Integrity tests', () => {
-  describe('package-lock.json', () => {
-    it('does not exist', async () => {
-      await expect(stat(`${process.cwd()}/package-lock.json`)).rejects.toThrowError();
-    });
-  });
-});
