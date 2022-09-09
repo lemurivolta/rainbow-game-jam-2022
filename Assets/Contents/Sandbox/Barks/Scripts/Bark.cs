f2c53297e80cfd60300ef5172c52202624d311f9
madiber <Madiber@users.noreply.github.com>
2022-09-09 22:54:25 +0200

    fix:  forgot a script

--- Assets/Contents/Sandbox/Barks/Scripts/Bark.cs
+++ Assets/Contents/Sandbox/Barks/Scripts/Bark.cs
@@ -1,10 +1,19 @@
-public enum CHARACTER { NPC, MARCELLA, MARIELLE, SARAH, YELENA }
+using UnityEngine;
+
+public enum CHARACTER { NPC, CLOE, MARIELLE, SARAH, YELENA }
 [System.Serializable]
 public class Bark
 {
     public CHARACTER character;
     public string line_ita;
     public string line_eng;
+    [HideInInspector] public bool canSkip = false;
+    public Transform targetTransform;
+    public Bark nextBark { get; set; }
+    public bool pressToSkip;
+    [HideInInspector] public bool nextSceneOnSkip;
+
+    [HideInInspector] public bool restartAtEnd = false;
 
     public void Play()
     {
@@ -13,6 +22,17 @@ public class Bark
 
     public string GetBark()
     {
-        return line_ita;
+        switch (LanguageManager.GetLanguage())
+        {
+            case Language.ITA:
+                return line_ita;
+                break;
+            case Language.ENG:
+                return line_eng;
+                break;
+            default:
+                return line_ita;
+                break;
+        }        
     }
 }
