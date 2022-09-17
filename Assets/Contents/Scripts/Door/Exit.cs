138753d1b21e4647e84ae265d8ab3c76a53849cb
Mattia Belletti <mattia.belletti@gmail.com>
2022-09-17 11:08:16 +0200

    chore: removed debug prints.

--- Assets/Contents/Scripts/Door/Exit.cs
+++ Assets/Contents/Scripts/Door/Exit.cs
@@ -22,14 +22,14 @@ public class Exit : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log($"In trigger! IsExit = {IsExit}, CanExit = {CanExit}, collision.gameObject = {collision.gameObject}, isci = {collision.gameObject.transform.parent.GetComponent<CharacterInfo>() != null}");
+        // Debug.Log($"In trigger! IsExit = {IsExit}, CanExit = {CanExit}, collision.gameObject = {collision.gameObject}, isci = {collision.gameObject.transform.parent.GetComponent<CharacterInfo>() != null}");
         // check if a character entered the exit zone
         if (IsExit &&
             CanExit &&
             collision.gameObject.transform
             .parent.GetComponent<CharacterInfo>() != null)
         {
-            Debug.Log("Go to next level");
+            // Debug.Log("Go to next level");
             SchermateManager.Instance.EndSchermata();
         }
     }
