b6a77599ffe06fed1d185c53f92d73480db18754
madiber <Madiber@users.noreply.github.com>
2022-09-19 13:37:59 +0200

    feature: language dependant label

---
+++ Assets/Contents/Sandbox/Language/LanguageDependentLabel.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LanguageDependentLabel : MonoBehaviour
+{
+    [SerializeField] TMP_Text label;
+    [SerializeField] string line_ita, line_eng;
+
+    private void Awake()
+    {
+        if (label == null)
+            label = GetComponent<TMP_Text>();
+    }
+
+    private void OnEnable()
+    {
+        SetText();
+    }
+
+    private void SetText()
+    {
+        switch (LanguageManager.GetLanguage())
+        {
+            case Language.ITA:
+                label.text = line_ita;
+                break;
+            case Language.ENG:
+                label.text = line_eng;
+                break;
+            default:
+                label.text = line_ita;
+                break;
+        }
+    }
+}
