e717525185294077f5e48f745d0acec6b6a98ffe
Mattia Belletti <mattia.belletti@gmail.com>
2022-09-22 19:12:32 +0200

    feat: new warning symbol.

--- Assets/Contents/Scripts/Enemy/EnemyDangerBalloonHandler.cs
+++ Assets/Contents/Scripts/Enemy/EnemyDangerBalloonHandler.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class EnemyDangerBalloonHandler : MonoBehaviour
 {
-    public GameObject[] Balloons;
-    public SpriteMask SpriteMask;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     public void OnDangerLevelChanged(float delta)
     {
-        foreach(var balloon in Balloons)
-        {
-            balloon.SetActive(delta > 0);
-        }
-        SpriteMask.alphaCutoff = 1 - delta;
+        // foreach(var balloon in Balloons)
+        // {
+        //     balloon.SetActive(delta > 0);
+        // }
+        // SpriteMask.alphaCutoff = 1 - delta;
+        Debug.Log($"danger delta is {delta}");
+        spriteRenderer.enabled = delta > 0;
+        animator.Play("warning", 0, delta);
     }
 }
