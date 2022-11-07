47316c1849df0771676154d89afbcb124dc0eaea
Mattia Belletti <mattia.belletti@gmail.com>
2022-11-07 21:29:42 +0100

    fix: remove characters not present in font

--- Assets/Contents/Fonts/Editor/BalloonTextExtractor.cs
+++ Assets/Contents/Fonts/Editor/BalloonTextExtractor.cs
@@ -1,9 +1,9 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
 using UnityEngine.Assertions;
 using System.Collections.Generic;
+using System;
 
 public class BalloonTextExtractor : EditorWindow
 {
@@ -16,33 +16,96 @@ public class BalloonTextExtractor : EditorWindow
 
     private TextField charactersLabel;
     private Label statusLabel;
+    private VisualElement buttonsContainer;
+    private ListView linesListView;
 
     public void CreateGUI()
     {
-        VisualElement root = rootVisualElement;
+        try
+        {
+            VisualElement root = rootVisualElement;
 
-        // Import UXML
-        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Contents/Fonts/Utilities/BalloonTextExtractor.uxml");
-        VisualElement content = visualTree.Instantiate();
-        root.Add(content);
+            // Import UXML
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Contents/Fonts/Editor/BalloonTextExtractor.uxml");
+            VisualElement content = visualTree.Instantiate();
+            root.Add(content);
 
-        // A stylesheet can be added to a VisualElement.
-        // The style will be applied to the VisualElement and all of its children.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Contents/Fonts/Utilities/BalloonTextExtractor.uss");
-        root.styleSheets.Add(styleSheet);
+            // A stylesheet can be added to a VisualElement.
+            // The style will be applied to the VisualElement and all of its children.
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Contents/Fonts/Editor/BalloonTextExtractor.uss");
+            root.styleSheets.Add(styleSheet);
 
-        // bind events
-        charactersLabel = root.Query<TextField>("characters").First();
-        Assert.IsNotNull(charactersLabel, "could not find characters label");
-        statusLabel = root.Query<Label>("status").First();
-        Assert.IsNotNull(statusLabel, "could not find status label");
-        root.Query<Button>("findButton").First().RegisterCallback<ClickEvent>((_) => FindCharacters());
+            // bind events
+            charactersLabel = root.Query<TextField>("characters").First();
+            Assert.IsNotNull(charactersLabel, "could not find characters label");
+            statusLabel = root.Query<Label>("status").First();
+            Assert.IsNotNull(statusLabel, "could not find status label");
+            root.Query<Button>("findButton").First().RegisterCallback<ClickEvent>((_) => FindCharacters());
+
+            // bind makeitem for listviews
+            buttonsContainer = root.Query<VisualElement>("buttonsContainer").First();
+            Assert.IsNotNull(buttonsContainer);
+            linesListView = root.Query<ListView>("linesListView").First();
+            Assert.IsNotNull(linesListView);
+            linesListView.makeItem = MakeLabel;
+            linesListView.bindItem = BindLabel;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.ToString());
+        }
     }
+
+    private VisualElement MakeButton()
+    {
+        Debug.Log("make button");
+        var button = new Button();
+        return button;
+    }
+
+    private void BindButton(VisualElement visualElement, int index)
+    {
+        Debug.Log($"Bind button to {visualElement} at index {index}");
+        var button = (Button)visualElement;
+        Assert.IsNotNull(button);
+        var ch = chars[index];
+        button.text = ch.ToString();
+        button.clicked += () =>
+        {
+            ShowLinesFor(ch);
+        };
+    }
+
+    private char currentlySelectedChar;
+
+    private void ShowLinesFor(char ch)
+    {
+        currentlySelectedChar = ch;
+        linesListView.itemsSource = stringsByCharacter[ch];
+        linesListView.Rebuild();
+    }
+
+    private VisualElement MakeLabel()
+    {
+        var label = new Label();
+        return label;
+    }
+
+    private void BindLabel(VisualElement label, int index)
+    {
+        var line = stringsByCharacter[currentlySelectedChar][index];
+        ((Label)label).text = line;
+    }
+
+    private List<char> chars;
+
+    private Dictionary<char, List<string>> stringsByCharacter = new();
 
     private void FindCharacters()
     {
         statusLabel.text = "Computing...";
         HashSet<char> set = new();
+        stringsByCharacter = new();
         var prefabGuids = AssetDatabase.FindAssets("t:prefab");
         foreach (var guid in prefabGuids)
         {
@@ -65,15 +128,32 @@ public class BalloonTextExtractor : EditorWindow
                             foreach (var ch in line)
                             {
                                 set.Add(ch);
+                                List<string> value = null;
+                                if (!stringsByCharacter.TryGetValue(ch, out value))
+                                {
+                                    value = new List<string>();
+                                    stringsByCharacter[ch] = value;
+                                }
+                                Assert.IsNotNull(value);
+                                value.Add(line);
                             }
                         }
                     }
                 }
             }
         }
-        var chars = new List<char>(set);
+        chars = new List<char>(set);
         chars.Sort();
         charactersLabel.value = string.Join("", chars);
         statusLabel.text = "";
+
+        buttonsContainer.Clear();
+        int i = 0;
+        foreach (var ch in chars)
+        {
+            var item = MakeButton();
+            BindButton(item, i++);
+            buttonsContainer.Add(item);
+        }
     }
 }
